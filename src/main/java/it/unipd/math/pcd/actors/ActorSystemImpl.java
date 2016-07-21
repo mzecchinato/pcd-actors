e2323f66ea805fd9f0301ff632ce521d7f07930a
Zekka491 <mattia.zecchinato@gmail.com>
2016-07-21 22:53:17 +0200

    Create ActorSystemImpl class

---
+++ src/main/java/it/unipd/math/pcd/actors/ActorSystemImpl.java
@@ -0,0 +1,22 @@
+package it.unipd.math.pcd.actors;
+
+/**
+ * Created by Mattia on 21/07/2016.
+ */
+public class ActorSystemImpl extends AbsActorSystem {
+
+    @Override
+    public void stop(ActorRef<?> actor) {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    protected ActorRef createActorReference(ActorMode mode) {
+        return null;
+    }
+}
